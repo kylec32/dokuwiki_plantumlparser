3be793ebe270f796700a800ad4688449c2bfefed
Kyle Carter <kylec32@gmail.com>
2018-10-03 22:01:39 -0600

    Back to testing2

--- syntax/PlantUmlDiagram.php
+++ syntax/PlantUmlDiagram.php
@@ -38,7 +38,7 @@ if (!class_exists('PlantUmlDiagram')) {
 
         private function stripSearchTags($text) {
             //return $text;
-            return str_replace("</span>", "", str_replace("<span class=\"search_hit\">", "", $text));
+            return str_replace("</span>", "", str_replace("span", "_____", $text));
         }
 
         private function encodep($text) {
